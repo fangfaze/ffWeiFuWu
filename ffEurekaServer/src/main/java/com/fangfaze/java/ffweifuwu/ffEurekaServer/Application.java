3c1eccb6d6c0e9e437715bbcb61b302e3f74e952
chenze <ace50@163.com>
2018-05-05 15:25:02 +0800

    eurekaServer

--- ffEurekaServer/src/main/java/com/fangfaze/java/ffweifuwu/ffEurekaServer/Application.java
+++ ffEurekaServer/src/main/java/com/fangfaze/java/ffweifuwu/ffEurekaServer/Application.java
@@ -2,19 +2,13 @@ package com.fangfaze.java.ffweifuwu.ffEurekaServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@RestController
+@EnableEurekaServer
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
-    @RequestMapping("/")
-    public String hello() {
-        return "hello1";
-
-    }
 }
